448c98946f8fc2058f38fa5aeb1bc5af42569a88
Maxim Buur <mail@maximbuur.nl>
2021-03-03 15:54:31 +0100

    Cleaner import, add style

--- projects/valtimo-portal/nl-material/src/lib/nl-material.module.ts
+++ projects/valtimo-portal/nl-material/src/lib/nl-material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ToolbarComponent } from './components/';
 
 @NgModule({
   imports: [

28fe28753c7b02e41d25e588376e49083ce8cd29
Maxim Buur <mail@maximbuur.nl>
2021-03-03 14:39:37 +0100

    Add toolbar component

--- projects/valtimo-portal/nl-material/src/lib/nl-material.module.ts
+++ projects/valtimo-portal/nl-material/src/lib/nl-material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NlMaterialComponent } from './nl-material.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [NlMaterialComponent],
+  declarations: [NlMaterialComponent, ToolbarComponent],
   imports: [
     MatToolbarModule
   ],

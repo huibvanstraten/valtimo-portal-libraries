746e5dabb3349b6983743d9780ebc155daaa0dc3
Maxim Buur <mail@maximbuur.nl>
2021-03-03 09:04:06 +0100

    Add lazy loaded modules for each route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'meldingen', loadChildren: () => import('./notifications/notifications.module').then(m => m.NotificationsModule)
+  },
+  {
+    path: 'aanvragen', loadChildren: () => import('./cases/cases.module').then(m => m.CasesModule)
+  },
+  {
+    path: 'taken', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}

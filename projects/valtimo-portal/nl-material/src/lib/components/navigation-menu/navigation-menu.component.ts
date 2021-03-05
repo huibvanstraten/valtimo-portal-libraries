1d1bf645d81ceb33c1aee3d1af54b9dcac8ef12f
Maxim Buur <mail@maximbuur.nl>
2021-03-05 11:24:56 +0100

    Menu with styling

--- projects/valtimo-portal/nl-material/src/lib/components/navigation-menu/navigation-menu.component.ts
+++ projects/valtimo-portal/nl-material/src/lib/components/navigation-menu/navigation-menu.component.ts
@@ -1,19 +1,34 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {NavigationMenuItem} from '../../interfaces';
+import {NavigationEnd, Router} from '@angular/router';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'nl-material-navigation-menu',
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.scss']
 })
-export class NavigationMenuComponent implements OnInit {
+export class NavigationMenuComponent implements OnInit, OnDestroy {
 
   @Input() items: Array<NavigationMenuItem> = [];
 
-  constructor() {
+  currentUrl$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  private routerSubscription: Subscription = Subscription.EMPTY;
+
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl$.next(event.url);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
   }
 
 }

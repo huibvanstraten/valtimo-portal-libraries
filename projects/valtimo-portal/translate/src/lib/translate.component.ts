f3e2a8243d445accfffe838dce30ec06bcbdead1
Maxim Buur <mail@maximbuur.nl>
2021-03-07 22:11:48 +0100

    Add translate library

---
+++ projects/valtimo-portal/translate/src/lib/translate.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-translate',
+  template: `
+    <p>
+      translate works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TranslateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/valtimo-portal/translate/src/lib/translate.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { TranslateComponent } from './translate.component';
+
+
+
+@NgModule({
+  declarations: [TranslateComponent],
+  imports: [
+  ],
+  exports: [TranslateComponent]
+})
+export class TranslateModule { }

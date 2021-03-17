8513d7b02e65887de570a2f466ef10d687f83143
Maxim Buur <mail@maximbuur.nl>
2021-03-17 15:48:57 +0100

    Add language selector

---
+++ projects/valtimo-portal/nl-material/src/lib/components/language-selector/language-selector.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'nl-material-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss']
+})
+export class LanguageSelectorComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}

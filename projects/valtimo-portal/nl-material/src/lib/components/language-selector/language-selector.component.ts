b426eb1a9fd90219c641a660d233ba93d13e2e8b
Maxim Buur <mail@maximbuur.nl>
2021-03-18 13:19:14 +0100

    Fix lang switch state

--- projects/valtimo-portal/nl-material/src/lib/components/language-selector/language-selector.component.ts
+++ projects/valtimo-portal/nl-material/src/lib/components/language-selector/language-selector.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {Observable} from "rxjs";
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from "rxjs";
 import {TranslateService} from "@ngx-translate/core";
 import {LocalizeRouterService} from "@gilsdav/ngx-translate-router";
 import {SidenavService} from "../../services";
@@ -10,7 +10,7 @@ import {LanguageSelectorMode} from "../../interfaces";
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.scss']
 })
-export class LanguageSelectorComponent {
+export class LanguageSelectorComponent implements OnInit, OnDestroy {
 
   @Input() locales: Array<string> = [];
 
@@ -23,11 +23,27 @@ export class LanguageSelectorComponent {
   readonly dropdownMode = LanguageSelectorMode.dropdown;
   readonly toggleMode = LanguageSelectorMode.toggleButtons;
 
-  constructor(private translateService: TranslateService, private localizeService: LocalizeRouterService, private sidenavService: SidenavService) {
+  private localeSubscription!: Subscription;
+
+  constructor(
+    private translateService: TranslateService,
+    private localizeService: LocalizeRouterService,
+    private sidenavService: SidenavService
+  ) {
     const currentLang = this.translateService.currentLang;
     this.selectedLocale = currentLang;
     this.sidenavService.currentLang = currentLang;
     this.open$ = this.sidenavService.open$;
+  }
+
+  ngOnInit(): void {
+    this.localeSubscription = this.sidenavService.currentLang$.subscribe((lang) => {
+      this.selectedLocale = lang;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.localeSubscription.unsubscribe();
   }
 
   useLanguage(language: string): void {

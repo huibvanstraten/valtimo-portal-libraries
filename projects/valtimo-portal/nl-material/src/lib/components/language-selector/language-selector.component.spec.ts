85a280ebe363a83ca9c479c78ce932ae56058429
Maxim Buur <mail@maximbuur.nl>
2021-03-23 14:30:37 +0100

    Add copyright

--- projects/valtimo-portal/nl-material/src/lib/components/language-selector/language-selector.component.spec.ts
+++ projects/valtimo-portal/nl-material/src/lib/components/language-selector/language-selector.component.spec.ts
@@ -1,25 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*
+ * Copyright 2015-2021 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import { LanguageSelectorComponent } from './language-selector.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {LanguageSelectorComponent} from './language-selector.component';
 
 describe('LanguageSelectorComponent', () => {
-  let component: LanguageSelectorComponent;
-  let fixture: ComponentFixture<LanguageSelectorComponent>;
+    let component: LanguageSelectorComponent;
+    let fixture: ComponentFixture<LanguageSelectorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LanguageSelectorComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LanguageSelectorComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LanguageSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LanguageSelectorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

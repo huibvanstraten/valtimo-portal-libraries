f3888e2d654bcc3add4faf4806807f2fc4570777
Maxim Buur <mail@maximbuur.nl>
2021-03-31 13:29:23 +0200

    Styling

--- projects/valtimo-portal/nl-material/src/lib/components/form-io/form-io.component.ts
+++ projects/valtimo-portal/nl-material/src/lib/components/form-io/form-io.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormioForm} from '@formio/angular';
 
 @Component({
   selector: 'nl-material-form-io',
   templateUrl: './form-io.component.html',
-  styleUrls: ['./form-io.component.scss']
+  styleUrls: ['./form-io.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FormIoComponent implements OnInit {
   @Input() definition!: FormioForm;

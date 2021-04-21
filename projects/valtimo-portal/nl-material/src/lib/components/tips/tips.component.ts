73825f22ee16e797bfa9a96cc2a9f205b7e1fb56
Maxim Buur <mail@maximbuur.nl>
2021-04-21 15:40:51 +0200

    Working cards for tips

--- projects/valtimo-portal/nl-material/src/lib/components/tips/tips.component.ts
+++ projects/valtimo-portal/nl-material/src/lib/components/tips/tips.component.ts
@@ -1,17 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {TipsService} from '../../services';
+import {Tip} from '../../interfaces';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {take, tap} from 'rxjs/operators';
+import {CardType} from '../../enums';
 
 @Component({
   selector: 'nl-material-tips',
   templateUrl: './tips.component.html',
   styleUrls: ['./tips.component.scss']
 })
-export class TipsComponent implements OnInit {
+export class TipsComponent {
+
+  index$ = new BehaviorSubject<number>(0);
+
+  tips$!: Observable<Array<Tip>>;
+
+  readonly tipType = CardType.tip;
 
   constructor(private readonly tipsService: TipsService) {
+    this.tips$ = this.tipsService.tips$
+      .pipe(
+        tap(() => this.index$.next(0))
+      );
   }
 
-  ngOnInit(): void {
+  next(): void {
+    this.index$.pipe(take(1)).subscribe((index) => {
+      this.index$.next(index + 1);
+    });
   }
 
+  previous(): void {
+    this.index$.pipe(take(1)).subscribe((index) => {
+      this.index$.next(index - 1);
+    });
+  }
 }

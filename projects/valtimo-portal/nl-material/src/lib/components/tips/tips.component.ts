241b1004cabd3f2b0590a2795389df46fd0a4de9
Maxim Buur <mail@maximbuur.nl>
2021-04-21 20:43:08 +0200

    Add tips to pages

--- projects/valtimo-portal/nl-material/src/lib/components/tips/tips.component.ts
+++ projects/valtimo-portal/nl-material/src/lib/components/tips/tips.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TipsService} from '../../services';
 import {Tip} from '../../interfaces';
-import {BehaviorSubject, combineLatest, Subscription} from 'rxjs';
-import {map, take, tap} from 'rxjs/operators';
+import {BehaviorSubject, combineLatest, interval, Subscription} from 'rxjs';
+import {map, switchMap, take, tap} from 'rxjs/operators';
 import {CardType} from '../../enums';
 import {LocalizeRouterService} from '@gilsdav/ngx-translate-router';
 import {Router} from '@angular/router';
@@ -20,11 +20,15 @@ export class TipsComponent implements OnInit, OnDestroy {
 
   tipsLength$ = new BehaviorSubject<number>(0);
 
-  tip$ = new Subject<Tip>;
+  // @ts-ignore
+  tip$ = new BehaviorSubject<Tip>(undefined);
 
   private tipsSubscription!: Subscription;
+  private intervalSubscription!: Subscription;
 
   readonly tipType = CardType.tip;
+
+  private readonly tipIntervalInSeconds: number = 15;
 
   constructor(
     private readonly tipsService: TipsService,
@@ -34,26 +38,26 @@ export class TipsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.tipsSubscription =
-      combineLatest([this.tipsService.tips$, this.index$])
-        .pipe(
-          tap(([tips]) => this.tipsLength$.next(tips.length)),
-          map(([tips, index]) => this.tip$.next(tips[index]))
-        )
-        .subscribe();
+    this.openTipsSubscription();
+    this.openIntervalSubscription();
   }
 
   ngOnDestroy(): void {
     this.tipsSubscription.unsubscribe();
+    this.closeIntervalSubscription();
   }
 
   next(): void {
+    this.closeIntervalSubscription();
+
     this.index$.pipe(take(1)).subscribe((index) => {
       this.index$.next(index + 1);
     });
   }
 
   previous(): void {
+    this.closeIntervalSubscription();
+
     this.index$.pipe(take(1)).subscribe((index) => {
       this.index$.next(index - 1);
     });
@@ -67,5 +71,33 @@ export class TipsComponent implements OnInit, OnDestroy {
 
   private getNewCaseRoute(): string {
     return `${this.localizeRouterService.translateRoute('/cases/newCase')}`;
+  }
+
+  private openTipsSubscription(): void {
+    this.tipsSubscription =
+      combineLatest([this.tipsService.tips$, this.index$])
+        .pipe(
+          tap(([tips]) => this.tipsLength$.next(tips.length)),
+          map(([tips, index]) => this.tip$.next(tips[index]))
+        )
+        .subscribe();
+  }
+
+  private openIntervalSubscription(): void {
+    this.intervalSubscription = interval(this.tipIntervalInSeconds * 1000)
+      .pipe(
+        switchMap(() => combineLatest([this.index$, this.tipsLength$]).pipe(take(1))),
+        tap(([index, length]) => {
+          if (index + 1 === length) {
+            this.index$.next(0);
+          } else {
+            this.index$.next(index + 1);
+          }
+        })
+      ).subscribe();
+  }
+
+  private closeIntervalSubscription(): void {
+    this.intervalSubscription.unsubscribe();
   }
 }

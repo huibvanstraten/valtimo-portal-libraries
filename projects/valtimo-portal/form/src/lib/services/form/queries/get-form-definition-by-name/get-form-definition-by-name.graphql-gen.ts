d3eda7d02736a9dea9393bcc93ff44eb64dc80ba
Maxim Buur <mail@maximbuur.nl>
2021-04-23 10:50:07 +0200

    Task service with query

--- projects/valtimo-portal/form/src/lib/services/form/queries/get-form-definition-by-name/get-form-definition-by-name.graphql-gen.ts
+++ projects/valtimo-portal/form/src/lib/services/form/queries/get-form-definition-by-name/get-form-definition-by-name.graphql-gen.ts
@@ -125,7 +125,7 @@ export const GetFormDefinitionByNameDocument = gql`
 }
     `;
 
-@Injectable({
+  @Injectable({
     providedIn: 'root'
   })
   export class GetFormDefinitionByNameGQL extends Apollo.Query<GetFormDefinitionByNameQuery, GetFormDefinitionByNameQueryVariables> {

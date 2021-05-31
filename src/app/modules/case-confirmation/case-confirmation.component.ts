a8e190012f7f91ac9c9158c8825fa0876b6c3634
Maxim Buur <mail@maximbuur.nl>
2021-05-31 13:30:30 +0200

    Fix routing

--- src/app/modules/case-confirmation/case-confirmation.component.ts
+++ src/app/modules/case-confirmation/case-confirmation.component.ts
@@ -15,14 +15,15 @@
  */
 
 import {Component} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
-  selector: 'app-case',
-  templateUrl: './case.component.html',
-  styleUrls: ['./case.component.scss']
+  selector: 'app-case-confirmation',
+  templateUrl: './case-confirmation.component.html',
+  styleUrls: ['./case-confirmation.component.scss']
 })
 export class CaseConfirmationComponent {
-
+  loading$ = new BehaviorSubject<boolean>(true);
 
   constructor() {
   }
--- src/app/modules/cases/cases-routing.module.ts
+++ src/app/modules/cases/cases-routing.module.ts
@@ -43,6 +43,15 @@ const routes: Routes = [
       title: 'TITLES.case',
       animation: 'DetailPage',
     }
+  },
+  {
+    path: 'caseConfirmation',
+    loadChildren: () => import('@case-confirmation/case-confirmation.module').then(m => m.CaseConfirmationModule),
+    canActivate: [KeycloakAppAuthGuard],
+    data: {
+      title: 'TITLES.caseConfirmation',
+      animation: 'DetailPage',
+    }
   }
 ];
 
